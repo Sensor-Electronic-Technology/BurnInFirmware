d74a379208961267ecf661e8c3731974b47f8f14
Andrew Elmendorf <aelmendorf234@gmail.com>
2024-01-05 09:51:38 -0500

    logger work

--- src/Logging/SerialLogger.hpp
+++ src/Logging/SerialLogger.hpp
@@ -1,6 +1,12 @@
 #pragma once
-//#include <SD.h>
+#include <SD.h>
 #include <avr/pgmspace.h>
+
+enum MessageType{
+    Error,
+    SystemMessage,
+    Data
+};
 
 
 class SerialLogger{
@@ -12,10 +18,35 @@ public:
         return instance;
     }
 
-    static void LogMessage(){
+    static void Log(MessageType messageType,const char *longLine){
+        auto instance=SerialLogger::Instance();
+        switch(messageType){
+            case MessageType::Data:{
+                break;
+            }
+            case MessageType::SystemMessage:{
+                break;
+            }
+            case MessageType::Error:{
+                break;
+            }
+        }
+    }
+
+    void print(const __FlashStringHelper* format,...){
         
+        char buffer[128];
+        PGM_P pointer=reinterpret_cast<PGM_P>(format);
+        va_list args;
+        va_start(args,format);
+        vsnprintf_P(buffer,sizeof(buffer),pointer,args);
+        va_end(args);
+        this->serialLog->println();
+
+
     }
 private:
     static SerialLogger* instance;
-    //File logFile;
+    File* logFile;
+    Print* serialLog;
 };
